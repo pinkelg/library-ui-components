98f52d2e5cf628846e8ed2e8947ddd8335c15f88
Pinkel Gurung <pinkelgurung@gmail.com>
2022-07-14 19:59:46 -0400

    log component: fix proper prefix

--- src/utils/logger/index.ts
+++ src/utils/logger/index.ts
@@ -1,30 +1,18 @@
 /* eslint-disable no-console */
-/* eslint-disable prefer-spread */
-
-// import { format } from 'date-fns';
-
-// const clientSideLogging = process.env.LOG_LEVEL;
-// const LOG_PREFIX = format(new Date(), 'yyyy-MM-dd HH:MM:ss');
-
-/* eslint-disable no-console */
-/* eslint-disable prefer-spread */
-
 import { format } from 'date-fns';
 
-const LOG_PREFIX = format(new Date(), 'yyyy-MM-dd HH:MM:ss');
+const DATE_TIME = format(new Date(), 'yyyy-MM-dd HH:MM:ss');
 
 class Logger {
-  static trace = console.trace.bind(window.console, `LOG - ${LOG_PREFIX} -`);
+  static trace = console.trace.bind(window.console, `TRACE - ${DATE_TIME} -`);
 
-  static info = console.info.bind(window.console, `LOG - ${LOG_PREFIX} -`);
+  static info = console.info.bind(window.console, `INFO - ${DATE_TIME} -`);
 
-  static log = console.log.bind(window.console, `LOG - ${LOG_PREFIX} -`);
+  static debug = console.debug.bind(window.console, `DEBUG - ${DATE_TIME} -`);
 
-  static debug = console.debug.bind(window.console, `LOG - ${LOG_PREFIX} -`);
+  static warn = console.warn.bind(window.console, `WARN - ${DATE_TIME} -`);
 
-  static warn = console.warn.bind(window.console, `LOG - ${LOG_PREFIX} -`);
-
-  static error = console.error.bind(window.console, `LOG - ${LOG_PREFIX} -`);
+  static error = console.error.bind(window.console, `ERROR - ${DATE_TIME} -`);
 }
 
 export { Logger };
